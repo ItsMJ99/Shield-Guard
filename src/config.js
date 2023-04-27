7022bf563e489836d2a4212df73b6c383b0db1a5
ItsMJ99 <madhav3jadhav@gmail.com>
2023-04-27 21:41:57 +0530

    Basic code for signup,signin,login added.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js";
-import { getFirestore, getDocs, collection, addDoc, deleteDoc, doc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-firestore.js";
+import { getFirestore, getDocs, collection, addDoc, deleteDoc, doc, onSnapshot, updateDoc, query, where, orderBy, serverTimestamp } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-firestore.js";
+import { getAuth, createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-firestore.js";
 // import { initializeApp } from "firebase/app";
-// import { getFirestore, getDocs, collection, addDoc } from "firebase/firestore";
+// import { getFirestore, getDocs, collection, addDoc, onAuthStateChanged } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyD5t1c_-5hy0mwAlmNngRI2QOnA8XJgYwg",
@@ -18,9 +19,13 @@ const app = initializeApp(firebaseConfig);
 
 //init service
 const db = getFirestore(app);
+const auth = getAuth(app);
 
 //refers to the collection to access
 const colRef = collection(db, 'society')
+
+//queries
+const q = query(colRef, where("owner", "==", "Peter"))//add --> , orderBy('owner', 'desc') after ") to order
 
 //fetch/real time data records from Firestore
 // getDocs(colRef)
@@ -35,7 +40,18 @@ const colRef = collection(db, 'society')
 //         console.log(error.message)
 //     })
 
-onSnapshot(colRef, (snapshot) => {
+//get a single document
+const docRef = doc(db, 'society', '3iK2mrwzaqml7OmN0wjC')
+// getDocs(docRef)
+//     .then((doc) => {
+//         console.log(doc.data(), doc.id)
+//     })
+onSnapshot(docRef, (doc) => {
+    console.log(doc.data(), doc.id)
+})
+
+//replace the colRef with q to get only a specific record else you will get entire records
+onSnapshot(q, (snapshot) => {
     let societies = []
     snapshot.docs.forEach((doc) => {
         societies.push({ ...doc.data(), id: doc.id })
@@ -64,6 +80,76 @@ delSocForm.addEventListener('submit', (e) => {
     deleteDoc(docRef).then(() => {
         delSocReq.reset()
     })
+})
+
+const dRef = doc(db, 'society', updateForm.id.value)//docRef
+updateDoc(dRef, {
+    owner: 'updated owner'
+}).then(() => {
+    updateForm.reset()
+})
+
+//Firebase Authentication
+
+//signup
+const signupForm = document.querySelector('.signup')
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const email = signupForm.email.value
+    const password = signupForm.password.value
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((cred) => {
+            console.log('User Created = ', cred.user)
+            signupForm.reset()
+        }).catch((err) => {
+            console.log(err.message)
+        })
+})
+
+//log in/out
+const logoutBtn = documnet.querySelector('.logout')
+logoutBtn.addEventListener('click', () => {
+    signOut(auth).then(() => {
+        console.log('User logged out ')
+    })
+        .catch((err) => {
+            console.log(err.message)
+        })
+})
+
+const loginForm = document.querySelector('.login')
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const email = loginForm.email.value
+    const password = loginForm.password.value
+    signInWithEmailAndPassword(auth, email, password)
+        .then((cred) => {
+            console.log('user logged in = ', cred.user)
+        })
+        .catch((err) => {
+            console.log(err.message)
+        })
+})
+
+//subscribe to auth changes that occur
+onAuthStateChange(auth, (user) => {
+    console.log('user state changed : ', user)
+})
+
+//from real time data records in firestore part
+const unsubCol = onSnapshot(q, (snapshot) => {
+    //take from firestore real time records part
+    //can also be done for unsubDoc
+    //and for unsubAuth
+})
+
+//unsubscribe from changes auth and db
+const unsubBtn = document.querySelector('.unsub')//unsub is btn class
+unsubBtn.addEventListener('click', () => {
+    console.log('unsubscribing')
+    unsubCol()
+    unsubDoc()
+    unsubAuth()
 })
 
 export { db };
