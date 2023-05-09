18742c47bdb5c6a26feb250fa61f7711cec48d8e
ItsMJ99 <madhav3jadhav@gmail.com>
2023-05-09 12:44:28 +0530

    doc upload and viewing func added.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js";
 import { getFirestore, getDocs, collection, addDoc, deleteDoc, doc, onSnapshot, updateDoc, query, where, orderBy, serverTimestamp } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-firestore.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-storage.js";
 //import { initializeApp } from "firebase/app";
 //import { getFirestore, getDocs, collection, addDoc, deleteDoc, doc, onSnapshot, updateDoc, query, where, orderBy, serverTimestamp } from "firebase/firestore";
 
@@ -19,6 +20,9 @@ const app = initializeApp(firebaseConfig);
 //init service
 const db = getFirestore(app);
 
+//init storage
+const storage = getStorage();
+
 //refers to the collection to access
 const colRef = collection(db, 'society registration requests')
 
@@ -26,31 +30,66 @@ const colRef = collection(db, 'society registration requests')
 const addSocReq = document.querySelector('.societyRegistration')
 addSocReq.addEventListener('submit', (e) => {
     e.preventDefault()
-    alert('here')
+    const file = addSocReq.ownerdoc.files[0];
+    const storageRef = ref(storage, 'Ownership Documents/' + file.name);
+    const uploadTask = uploadBytes(storageRef, file);
+
+    uploadTask
+        .then((snapshot) => {
+            console.log('File uploaded successfully.');
+
+            // Get the file URL
+            const fileRef = ref(storage, snapshot.ref.fullPath);
+            getDownloadURL(fileRef)
+                .then((url) => {
+                    // Store the file URL in Firestore
+                    addDoc(collection(db, 'society registration requests'), {
+                        owner_fname: addSocReq.o_fname.value,
+                        owner_mname: addSocReq.o_mname.value,
+                        owner_lname: addSocReq.o_lname.value,
+                        owner_email: addSocReq.o_email.value,
+                        owner_password: addSocReq.o_password.value,
+                        owner_confirm_password: addSocReq.o_confirm_password.value,
+                        owner_phoneno: addSocReq.o_phoneno.value,
+                        owner_gender: addSocReq.o_gender.value,
+                        owner_dob: addSocReq.o_dob.value,
+                        owner_address: addSocReq.o_address.value,
+                        owner_state: addSocReq.o_state.value,
+                        owner_district: addSocReq.o_district.value,
+                        owner_pincode: addSocReq.o_pincode.value,
+                        society_name: addSocReq.s_name.value,
+                        society_address: addSocReq.s_address.value,
+                        society_state: addSocReq.s_state.value,
+                        society_district: addSocReq.s_district.value,
+                        society_pincode: addSocReq.s_pincode.value,
+                        society_document: url,
+                        society_email: addSocReq.s_email.value,
+                        society_phoneno: addSocReq.s_phoneno.value,
+                        society_FoundingDay: addSocReq.s_date.value,
+                        society_admins: addSocReq.s_admins.value
+                    })
+                        .then(() => {
+                            console.log('File URL stored in Firestore successfully.');
+                        })
+                        .catch((error) => {
+                            console.error('Error storing file URL in Firestore:', error);
+                        });
+                })
+                .catch((error) => {
+                    console.error('Error getting file URL:', error);
+                });
+        })
+        .catch((error) => {
+            console.error('Error uploading file:', error);
+        });
+})
+
+
+
+addSocReq.addEventListener('submit', (e) => {
+    e.preventDefault()
     addDoc(colRef, {
-        owner_fname: addSocReq.o_fname.value,
-        owner_mname: addSocReq.o_mname.value,
-        owner_lname: addSocReq.o_lname.value,
-        owner_email: addSocReq.o_email.value,
-        owner_password: addSocReq.o_password.value,
-        owner_confirm_password: addSocReq.o_confirm_password.value,
-        owner_phoneno: addSocReq.o_phoneno.value,
-        owner_gender: addSocReq.o_gender.value,
-        owner_dob: addSocReq.o_dob.value,
-        owner_address: addSocReq.o_address.value,
-        owner_state: addSocReq.o_state.value,
-        owner_district: addSocReq.o_district.value,
-        owner_pincode: addSocReq.o_pincode.value,
-        society_name: addSocReq.s_name.value,
-        society_address: addSocReq.s_address.value,
-        society_state: addSocReq.s_state.value,
-        society_district: addSocReq.s_district.value,
-        society_pincode: addSocReq.s_pincode.value,
-        society_document: addSocReq.o_doc.value,
-        society_email: addSocReq.s_email.value,
-        society_phoneno: addSocReq.s_phoneno.value,
-        society_FoundingDay: addSocReq.s_date.value,
-        society_admins: addSocReq.s_admins.value
+        society_document: url,
     }).then(() => {
         addSocReq.reset()
     })
