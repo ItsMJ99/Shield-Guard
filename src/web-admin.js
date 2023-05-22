c2b90702286f70764d0dcf5030de069f01451d38
ItsMJ99 <madhav3jadhav@gmail.com>
2023-05-22 07:49:00 +0530

    bug fixes 2

--- src/web-admin.js
+++ src/web-admin.js
@@ -31,79 +31,88 @@ const db = getFirestore(app);
 //     console.log('user state changed : ', user)
 // })
 
-//refers to the collection to access
-const regSocieties = collection(db, 'registered societies')
-
 //add records to Firestore
-// const regSoc = document.querySelector('#admin-reg-res')
-// regSoc.addEventListener('submit', (e) => {
-//     e.preventDefault()
-//     const file = regSoc.ownerdoc.files[0];
-//     const storageRef = ref(storage, 'Ownership Documents/' + file.name);
-//     const uploadTask = uploadBytes(storageRef, file);
-//     uploadTask
-//         .then((snapshot) => {
-//             console.log('File uploaded successfully.');
+const regSoc = document.querySelector('#admin-reg-res')
+regSoc.addEventListener('submit', (e) => {
+    e.preventDefault()
+    alert('yolo');
+    const file = regSoc.ownerdoc.files[0];
+    const storageRef = ref(storage, 'Ownership Documents/' + file.name);
+    const uploadTask = uploadBytes(storageRef, file);
+    uploadTask
+        .then((snapshot) => {
+            console.log('File uploaded successfully.');
 
-//             // Get the file URL
-//             const fileRef = ref(storage, snapshot.ref.fullPath);
-//             getDownloadURL(fileRef)
-//                 .then((url) => {
-//                     // Store the file URL in Firestore
-//                     addDoc(collection(db, 'registered societies'), {//collection(db, 'society registration requests')
-//                         owner_fname: regSoc.o_fname.value,
-//                         owner_mname: regSoc.o_mname.value,
-//                         owner_lname: regSoc.o_lname.value,
-//                         owner_email: regSoc.o_email.value,
-//                         owner_phoneno: regSoc.o_phoneno.value,
-//                         owner_password: regSoc.o_password.value,
-//                         owner_confirm_password: regSoc.o_confirm_password.value,
-//                         owner_gender: regSoc.o_gender.value,
-//                         owner_occupation: regSoc.o_occupation.value,
-//                         owner_dob: regSoc.o_dob.value,
-//                         owner_address: regSoc.o_address.value,
-//                         owner_state: regSoc.o_state.value,
-//                         owner_district: regSoc.o_district.value,
-//                         owner_pincode: regSoc.o_pincode.value,
-//                         society_name: regSoc.s_name.value,
-//                         society_address: regSoc.s_address.value,
-//                         society_state: regSoc.s_state.value,
-//                         society_district: regSoc.s_district.value,
-//                         society_pincode: regSoc.s_pincode.value,
-//                         society_document: url,
-//                         society_phoneno: regSoc.s_phoneno.value,
-//                         society_FoundingDay: regSoc.s_date.value,
-//                         society_admins: regSoc.s_admins.value,
-//                         society_email: regSoc.s_email.value
-//                     })
-//                         .then(() => {
-//                             regSoc.reset()
-//                             console.log('File URL stored in Firestore successfully.');
-//                         })
-//                         .catch((error) => {
-//                             console.error('Error storing file URL in Firestore:', error);
-//                         });
-//                 })
-//                 .catch((error) => {
-//                     console.error('Error getting file URL:', error);
-//                 });
-//         })
-//         .catch((error) => {
-//             console.error('Error uploading file:', error);
-//         });
-// })
+            // Get the file URL
+            const fileRef = ref(storage, snapshot.ref.fullPath);
+            getDownloadURL(fileRef)
+                .then((url) => {
+                    // Store the file URL in Firestore
+                    addDoc(collection(db, 'registered societies'), {//collection(db, 'society registration requests')
+                        owner_fname: regSoc.o_fname.value,
+                        owner_mname: regSoc.o_mname.value,
+                        owner_lname: regSoc.o_lname.value,
+                        owner_email: regSoc.o_email.value,
+                        owner_phoneno: regSoc.o_phoneno.value,
+                        owner_password: regSoc.o_password.value,
+                        owner_gender: regSoc.o_gender.value,
+                        owner_occupation: regSoc.o_occupation.value,
+                        owner_dob: regSoc.o_dob.value,
+                        owner_address: regSoc.o_address.value,
+                        owner_state: regSoc.o_state.value,
+                        owner_district: regSoc.o_district.value,
+                        owner_pincode: regSoc.o_pincode.value,
+                        society_name: regSoc.s_name.value,
+                        society_address: regSoc.s_address.value,
+                        society_state: regSoc.s_state.value,
+                        society_district: regSoc.s_district.value,
+                        society_pincode: regSoc.s_pincode.value,
+                        society_document: url,
+                        society_phoneno: regSoc.s_phoneno.value,
+                        society_FoundingDay: regSoc.s_date.value,
+                        society_admins: regSoc.s_admins.value,
+                        society_email: regSoc.s_email.value
+                    })
+                        .then(() => {
+                            regSoc.reset()
+                            console.log('File URL stored in Firestore successfully.');
+                        })
+                        .catch((error) => {
+                            console.error('Error storing file URL in Firestore:', error);
+                        });
+                })
+                .catch((error) => {
+                    console.error('Error getting file URL:', error);
+                });
+        })
+        .catch((error) => {
+            console.error('Error uploading file:', error);
+        });
 
-//refers to the collection to access
-const colRef = collection(db, 'society registration requests')
+
+    //get user info
+    const email = regSoc.s_email.value;
+    const password = regSoc.o_password.value;
+    console.log(email, password);
+    //signup user
+    createUserWithEmailAndPassword(webAdminAuth, email, password).then(cred => {
+        console.log(cred.user);
+        regSoc.reset();
+    });
+    alert('Society Registered and account created Successfully');
+})
+
+
 
 // const table = document.getElementsByClassName('table datatable');
-// //const table = document.getElementById('society-records');
 
 // document.addEventListener('DOMContentLoaded', () => {
 //     // The script code goes here
 
 //     getDocs(colRef)
 //         .then((snapshot) => {
+//             let tableRows = '';
+
 //             snapshot.docs.forEach((doc) => {
 //                 const data = doc.data();
 //                 const ownername = data.owner_fname + " " + data.owner_mname + " " + data.owner_lname;
@@ -112,80 +121,32 @@ const colRef = collection(db, 'society registration requests')
 //                 const ownerdob = data.owner_dob;
 //                 const ownerfoundingday = data.society_FoundingDay;
 
-//                 // Create table row and cells
-//                 const row = document.createElement('tr');
-//                 const ownernameCell = document.createElement('td');
-//                 ownernameCell.textContent = ownername;
-//                 const socnameCell = document.createElement('td');
-//                 socnameCell.textContent = socname;
-//                 const owneroccupationCell = document.createElement('td');
-//                 owneroccupationCell.textContent = owneroccupation;
-//                 const ownerdobCell = document.createElement('td');
-//                 ownerdobCell.textContent = ownerdob;
-//                 const ownerfoundingdayCell = document.createElement('td');
-//                 ownerfoundingdayCell.textContent = ownerfoundingday;
+//                 const row = `<tr>
+//                                 <td>${ownername}</td>
+//                                 <td>${socname}</td>
+//                                 <td>${owneroccupation}</td>
+//                                 <td>${ownerdob}</td>
+//                                 <td>${ownerfoundingday}</td>
+//                             </tr>`;
 
-//                 // Append cells to the row
-//                 row.appendChild(ownernameCell);
-//                 row.appendChild(socnameCell);
-//                 row.appendChild(owneroccupationCell);
-//                 row.appendChild(ownerdobCell);
-//                 row.appendChild(ownerfoundingdayCell);
-
-//                 // Append the row to the table
-//                 //var tab = document.createElement('table');
-//                 alert(table);
-//                 table.appendChild(row);
+//                 tableRows += row;
 //             });
+
+//             if (table) {
+//                 const tbody = table.querySelector('tbody');
+//                 if (tbody) {
+//                     tbody.innerHTML = tableRows;
+//                 } else {
+//                     console.log("Table body element not found");
+//                 }
+//             } else {
+//                 console.log("Table element not found");
+//             }
 //         })
 //         .catch((error) => {
 //             console.log(error.message);
 //         });
 // });
-
-const table = document.getElementsByClassName('table datatable');
-
-document.addEventListener('DOMContentLoaded', () => {
-    // The script code goes here
-
-    getDocs(colRef)
-        .then((snapshot) => {
-            let tableRows = '';
-
-            snapshot.docs.forEach((doc) => {
-                const data = doc.data();
-                const ownername = data.owner_fname + " " + data.owner_mname + " " + data.owner_lname;
-                const socname = data.society_name;
-                const owneroccupation = data.owner_occupation;
-                const ownerdob = data.owner_dob;
-                const ownerfoundingday = data.society_FoundingDay;
-
-                const row = `<tr>
-                                <td>${ownername}</td>
-                                <td>${socname}</td>
-                                <td>${owneroccupation}</td>
-                                <td>${ownerdob}</td>
-                                <td>${ownerfoundingday}</td>
-                            </tr>`;
-
-                tableRows += row;
-            });
-
-            if (table) {
-                const tbody = table.querySelector('tbody');
-                if (tbody) {
-                    tbody.innerHTML = tableRows;
-                } else {
-                    console.log("Table body element not found");
-                }
-            } else {
-                console.log("Table element not found");
-            }
-        })
-        .catch((error) => {
-            console.log(error.message);
-        });
-});
 
 
 
